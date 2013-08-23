1374781a30311cc23cee21d762d47702981e9ff6
oleks <oleks@diku.dk>
2013-08-23 21:36:29 +0200

    initialized repo

---
+++ README.md
@@ -0,0 +1,15 @@
+Dette er en samling af kantinens skilte og en skilteskabelon. Redigér et
+eksisterende skilt, eller lav et nyt i et passende underkatalog, og brug
+weave.sh til at smoke dit kantineskilt.
+
+## Licens
+
+Copyright (c) 2013 Datalogisk Kantineforening
+Licenseret under EUPL, version 1.1 udelukkende.
+Licensteksten: https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+
+## Kodestil
+
+Dokumentation og kommentarer skrives på dansk for hyggens skyld. Variabelnavne,
+filnavne, og andet er på engelsk for ikke at forstyrre de ellers engelske
+nøgleord og konventioner.
---
+++ weave.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+#
+# Copyright (c) 2013 Datalogisk Kantineforening
+# Licenseret under EUPL, version 1.1 udelukkende.
+# Licensteksten: https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+
+function help {
+  usage
+  echo "Dette skalprogram smoker dit kantineskilt."
+  echo ""
+  echo "Som udgangspunkt smokes der et portrætskilt med dansk hoved og fod"
+  echo "ud til ./output/skilt.pdf. Du har dog andre muligheder:"
+  echo ""
+  echo "  -l          For et vandret skilt"
+  echo "  -e          For english header and footer"
+  echo "  -o [FILE]   For at angive et navn andet end 'skilt'."
+  echo "  -h          For at se denne tekst."
+}
+
+function usage {
+  echo "Usage: $0 [OPTIONS] [FILE]"
+}
+
+function usage_try {
+  usage
+  echo "Try '$0 -h' for more information."
+}
+
+PORTRAIT="true"
+DANISH="true"
+JOBNAME="skilt"
+
+while getopts ":helo:" option; do
+  case "$option" in
+    l)  PORTRAIT="false" ;;
+    e)  DANISH="false" ;;
+    o)  JOBNAME="$OPTARG" ;;
+    h)  help
+        exit 0
+        ;;
+    :)  echo "Error: option requires an argument."
+        usage_try
+        exit 1
+        ;;
+    ?)  echo "Error: unknown option option."
+        usage_try
+        exit 1
+        ;;
+  esac
+done
+
+# Find det sidste argument:
+for TEXTNAME; do true; done
+if [ -z $TEXTNAME ]; then
+  echo "Error: you must specify an input file."
+  usage_try
+  exit 1
+fi
+
+BASENAME="main.tex"
+
+if [ ! -f $BASENAME ]; then
+    echo "Error: \"$BASENAME\" doesn't seem to be a file."
+    exit 1
+fi
+
+if [ ! -f $TEXTNAME ]; then
+    echo "Error: \"$TEXTNAME\" doesn't seem to be a file."
+    exit 1
+fi
+
+echo $BASENAME
+echo $TEXTNAME
+
+# Fjern filendelserne:
+BASENAME="${BASENAME%.*}"
+TEXTNAME="${TEXTNAME%.*}"
+
+
+OUTDIR="./output/"
+mkdir -p $OUTDIR
+
+COMMAND=$( cat <<EOF
+\\newif\\ifportrait\\portrait$PORTRAIT
+\\newif\\ifdanish\\danish$DANISH
+\\newcommand\\content[0]{\\input{$TEXTNAME}}\\input{$BASENAME}
+EOF
+)
+
+pdflatex -output-directory=$OUTDIR -file-line-error \
+  -jobname $JOBNAME \
+  $COMMAND
+
